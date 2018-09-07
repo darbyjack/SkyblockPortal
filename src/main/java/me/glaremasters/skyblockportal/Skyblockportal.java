2203f0ec7870e42c16e91bff3178b7e9461ab4d3
darbyjack <admin@glaremasters.me>
2018-09-07 13:24:11 -0500

    Finish plugin

--- src/main/java/me/glaremasters/skyblockportal/Skyblockportal.java
+++ src/main/java/me/glaremasters/skyblockportal/Skyblockportal.java
@@ -1,17 +1,30 @@
 package me.glaremasters.skyblockportal;
 
+import com.wasteofplastic.askyblock.ASkyBlockAPI;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Skyblockportal extends JavaPlugin {
+public final class Skyblockportal extends JavaPlugin implements Listener {
+
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(this, this);
     }
+
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
+    }
+
+    @EventHandler
+    public void onTeleport(PlayerTeleportEvent event) {
+        Player player = event.getPlayer();
+        if (event.getCause() != PlayerTeleportEvent.TeleportCause.NETHER_PORTAL) return;
+        if (ASkyBlockAPI.getInstance().getIslandLevel(player.getUniqueId()) < 1000) event.setCancelled(true);
     }
 }
